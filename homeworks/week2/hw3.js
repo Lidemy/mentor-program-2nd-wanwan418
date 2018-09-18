1f6d9737bb502ed363d1304bd9cafb2a082981a6
julia <skylife21666@gmail.com>
2018-09-18 20:22:42 +0800

    finish hw1 ~ hw4

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,12 @@
 function isPrime(n) {
-  
+	if(n < 2) return false
+	for(var i  = 2; i * i <= n; i++) {
+		if(n % i == 0) {
+			return false
+		} else {
+			return true
+		}
+	}
 }
 
 module.exports = isPrime
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,5 +1,32 @@
 function add(a, b) {
-  
+ // var result = []
+ // var a = ''
+ // var b = ''
+ // for(var i = 1 , i < a.length, i++) {
+ // 	carry = result[i] / 10
+ // 	result = a[i] + b[i] + carry
+ // 	result[i]% = 10
+
+ // }
+  var carry = 0, result = [],
+      len = Math.max(a.length, b.length), i = len;
+  while (i--) {
+    var sum = (+a[i - len + a.length]||0) + (+b[i - len + b.length]||0) + carry;
+    carry = parseInt(sum / 10);
+    result.unshift(sum % 10);
+  }
+  // if (carry) result.unshift(carry);
+  // return result.join('');
 }
+
+
+for(var i = 0; i = a.length - 1; i++) {
+	var result = a[i] + b[i]
+	if (result >= 10) {
+		result.split(1)
+	} 
+
+}
+console.log(result)
 
 module.exports = add;
