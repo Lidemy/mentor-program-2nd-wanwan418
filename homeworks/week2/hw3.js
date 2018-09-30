74c0dd82712952ee1d9b5e9764869fdf90560ada
julia <skylife21666@gmail.com>
2018-09-30 17:13:37 +0800

    modify hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,12 +1,12 @@
 function isPrime(n) {
-	if(n < 2) return false
-	for(var i  = 2; i * i <= n; i++) {
-		if(n % i == 0) {
+	if(n <= 1) return false
+
+	for(var i = 2; i < n ; i++) {
+		if(n % i === 0) {
 			return false
-		} else {
-			return true
 		}
 	}
+	return true
 }
 
 module.exports = isPrime
